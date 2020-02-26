881eb094ba1e033fb5040e98d2cd677dfacdf3c1
飯島 <himekawamaki45@gmail.com>
2020-02-26 01:44:29 +0000

    Finish japanization-and-time-format

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,8 @@ require_relative 'application'
 
 # Initialize the Rails application.
 Rails.application.initialize!
+
+
+Willpaginate::ViewHelpers.pagination_options[:previous_label] = '前ページ'
+
+Willpaginate::ViewHelpers.pagination_options[:next_label] = '次ページ'
